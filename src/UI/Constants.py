ccbb15765082368070fd707e8e2b5f3c10ed7c82
Viggó <viggoh21@ru.is>
2023-12-05 17:43:49 +0000

    Settum inn UI constants og gerðum eh models :dizzy_face:

--- src/UI/Constants.py
+++ src/UI/Constants.py
@@ -1,2 +1,53 @@
-class UIConstants():
+class UIConstants:
+    BOOKING_SYSTEM = "Booking System"
+    MANAGE_EMPLOYEES = "Manage Employees"
+    MANAGE_AIRPLANES = "Manage Airplanes"
+    MANAGE_DESTINATIONS = "Manage Destinations"
+    MANAGE_VOYAGES = "Manage Voyages"
+
+    DISPLAY_AIRPLANES = "Display Airplanes"
+    FIND_AIRPLANE = "Find Airplane"
+    PRINT_AIRPLANE_EFFICIENCY = "Print Airplane Efficiency"
     
+
+    REGISTER_NEW_EMPLOYEES = "Register New Employee"
+    
+
+    REGISTER_NEW_VOYAGE = "Register New Voyage"
+    EDIT_VOYAGE = "Edit Voyage"
+    POPULATE_VOYAGE = "Populate Voyage"
+    DISPLAY_VOYAGES = "Display Voyages"
+    CHECK_VOYAGE_STATUS = "Check Voyage Status"
+
+    
+    
+    
+    INVALID_INPUT = "Invalid input! Please try again."
+    QUIT_MESSAGE = "Bye Bye!"
+    DASH_SYMBOL = "-"
+    LENGTH_SYMBOL = 30
+    QUIT = "quit"
+    BACK = "back"
+
+
+    HEADER = (
+    f"{DASH_SYMBOL * LENGTH_SYMBOL}"
+    + "\n"
+    + "  NaN Air - {}"
+    + "\n"
+    + f"{DASH_SYMBOL * LENGTH_SYMBOL}"
+    )
+
+    MAIN_MENU = (
+
+        "1. {}"
+        + "\n"
+        + "2. {}"
+        + "\n"
+        + "3. {}"
+        + "\n"
+        + "4. {}"
+        + "\n"
+        + "q. {}"
+    )
+
--- src/main.py
+++ src/main.py
@@ -2,4 +2,3 @@ from UI.ui import MainMenu
 
 ui = MainMenu()
 ui.input_prompt_mainmenu()
-
