ab56521959d1dce8c34d6bc5dac82aabd659a929
Andrea <144338687+andreahrj@users.noreply.github.com>
2023-12-05 14:56:45 +0000

    PRETTYTABLE 🦋

--- src/UI/employees.py
+++ src/UI/employees.py
@@ -3,7 +3,7 @@ INVALID_INPUT = "Invalid input! Please try again."
 from prettytable import PrettyTable
 
 HEADER_EMPLOYEES = (
-      "------------------------------"
+    "------------------------------"
     + "\n"
     + "  NaN Air - {}"
     + "\n"
@@ -61,6 +61,7 @@ HEADER_INPUT_5 =(
     + "Employee Name, Employee SSID, Job Title, Pilot License (If applicable), Home Address, Phone Number, E-mail Address, Home Phone Number (optional)"
 )"""
 
+
 class Employees:
     def __init__(self, logic_wrapper):
         self.logic_wrapper = logic_wrapper
@@ -73,7 +74,6 @@ class Employees:
         print("b. Back")
         print("q. Quit")
 
-
     def input_prompt_employees(self):
         while True:
             command = input("User Input: ")
@@ -81,7 +81,7 @@ class Employees:
 
             if command == "q" or command == "q.":
                 print("Bye Bye!")
-                break           
+                break
 
             elif command == "b" or command == "b.":
                 break
@@ -90,58 +90,88 @@ class Employees:
                 self.list_employees()
 
             elif command == "2" or command == "2.":
-                pass             #eftir að implementa
-                               
+                pass  # eftir að implementa
+
             elif command == "3" or command == "3.":
-                pass             #eftir að implementa
-                                
+                pass  # eftir að implementa
+
             else:
                 print(INVALID_INPUT)
 
-
     def list_employees(self):
-        print(HEADER_EMPLOYEES.format("Employee list"))
-        all_employees = self.logic_wrapper.get_all_employees() # ÞETTA VIRKAR !! YAAAAY!!
-        if all_employees:
-            for employee in all_employees:
+        employees = self.logic_wrapper.get_all_employees()
 
-                print(employee.get("name"), employee.get("ssid"), employee.get("job_title"), 
-                      employee.get("license"), employee.get("address"), employee.get("phone_number"), 
-                      employee.get("e-mail_address"), employee.get("home_phone"), sep=", ")
+        if employees:
+            table = PrettyTable()
+            table.field_names = [
+                "Name",
+                "SSID",
+                "Job Title",
+                "License",
+                "Address",
+                "Phone Number",
+                "E-mail Address",
+            ]
 
+            for employee in employees:
+                table.add_row(
+                    [
+                        employee.get("name"),
+                        employee.get("ssid"),
+                        employee.get("job_title"),
+                        employee.get("license"),
+                        employee.get("address"),
+                        employee.get("phone_number"),
+                        employee.get("e-mail_address"),
+                    ]
+                )
+
+            print(table)
         else:
             print("No employees found.")
 
+    # def list_employees(self):
+    #     print(HEADER_EMPLOYEES.format("Employee list"))
+    #     all_employees = self.logic_wrapper.get_all_employees() # ÞETTA VIRKAR !! YAAAAY!!
+    #     if all_employees:
+    #         for employee in all_employees:
 
-        print("1. Search")
-        print("2. Sort by:")
-        command = input("User Input: ")
+    #             print(employee.get("name"), employee.get("ssid"), employee.get("job_title"),
+    #                   employee.get("license"), employee.get("address"), employee.get("phone_number"),
+    #                   employee.get("e-mail_address"), employee.get("home_phone"), sep=", ")
 
-        if command == "2" or command == "2.":
-            print("1. Captains")
-            print("2. Co-Pilots")
-            print("3. Flight Attendants")
-            print("4. Heads of Service")
+    #     else:
+    #         print("No employees found.")
 
-            self.get_sorted_list((input("User Input: ")))
+    #     print("1. Search")
+    #     print("2. Sort by:")
+    #     command = input("User Input: ")
 
-        elif command == "1" or command == "1.":
-            search_output = self.logic_wrapper.search(input("")) # implementa search hér - sjá logic.py
-            print(search_output)
+    #     if command == "2" or command == "2.":
+    #         print("1. Captains")
+    #         print("2. Co-Pilots")
+    #         print("3. Flight Attendants")
+    #         print("4. Heads of Service")
+
+    #         self.get_sorted_list((input("User Input: ")))
+
+    #     elif command == "1" or command == "1.":
+    #         search_output = self.logic_wrapper.search(input("")) # implementa search hér - sjá logic.py
+    #         print(search_output)
 
     def get_sorted_list(self, command):
         self.command = command
 
-        if command == "1" or command == "1." :
+        if command == "1" or command == "1.":
             pass
-        
+
         elif command == "2" or command == "2.":
             pass
-        
+
         elif command == "3" or command == "3.":
             pass
 
-        elif command == "4" or command =="4.":
+        elif command == "4" or command == "4.":
             pass
 
         else:
