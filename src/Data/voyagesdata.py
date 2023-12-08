b9dd49963f0a119a005d6323ec316229639535db
Andrea <144338687+andreahrj@users.noreply.github.com>
2023-12-08 15:05:31 +0000

    Update VoyagesData

--- src/Data/voyagesdata.py
+++ src/Data/voyagesdata.py
@@ -2,26 +2,58 @@ from Model.VoyageModel import Voyage
 
 import csv
 
+
 class VoyageData:
     def __init__(self):
         self.file_name = "src/Files/voyages.csv"
-        
+
     def get_all_voyages(self):
         ret_list = []
-        with open(self.file_name, newline="", encoding="utf-8") as csvfile: 
+        with open(self.file_name, newline="", encoding="utf-8") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                ret_list.append(Voyage(row["vid"], row["destination"], row["departuretime"], row["departuredate"], row["arrivaltime"], row["arrivaldate"], row["captain"], row["copilot"], row["flight_service_manager"], row["flight_attendant"]))
+                ret_list.append(
+                    Voyage(
+                        row["vid"],
+                        row["destination"],
+                        row["departuretime"],
+                        row["departuredate"],
+                        row["arrivaltime"],
+                        row["arrivaldate"],
+                        row["captain"],
+                        row["copilot"],
+                        row["flight_service_manager"],
+                        row["flight_attendant"],
+                    )
+                )
             return ret_list
-        
+
     def register_new_voyage(self, voyage):
-        with open(self.file_name, "a") as csvfile:
-            fieldnames = ["vid", "destination", "departuretime", "departuredate", "arrivaltime", "arrivaldate", "captain", "copilot", "flight_service_manager", "flight_attendant"]
+        with open(self.file_name, "a", newline="", encoding="utf-8") as csvfile:
+            fieldnames = [
+                "vid",
+                "destination",
+                "departuretime",
+                "departuredate",
+                "arrivaltime",
+                "arrivaldate",
+                "captain",
+                "copilot",
+                "flight_service_manager",
+                "flight_attendant",
+            ]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow({"vid": voyage.vid, "destination": voyage.destination, "departuretime": voyage.departuretime, "departuredate": voyage.departuredate, "arrivaltime": voyage.arrivaltime, "arrivaldate": voyage.arrivaldate, "captain": voyage.captain, "copilot": voyage.copilot, "flight_service_manager": voyage.flight_service_manager, "flight_attendant": voyage.flight_attendant})
-            
-    def register_new_voyage(self, voyage):
-        with open(self.file_name, "a") as csvfile:
-            fieldnames = ["vid", "destination", "departuretime", "departuredate", "arrivaltime", "arrivaldate", "captain", "copilot", "flight_service_manager", "flight_attendant"]
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow({"vid": voyage.vid, "destination": voyage.destination, "departuretime": voyage.departuretime, "departuredate": voyage.departuredate, "arrivaltime": voyage.arrivaltime, "arrivaldate": voyage.arrivaldate, "captain": voyage.captain, "copilot": voyage.copilot, "flight_service_manager": voyage.flight_service_manager, "flight_attendant": voyage.flight_attendant})
+            writer.writerow(
+                {
+                    "vid": voyage.vid,
+                    "destination": voyage.destination,
+                    "departuretime": voyage.departuretime,
+                    "departuredate": voyage.departuredate,
+                    "arrivaltime": voyage.arrivaltime,
+                    "arrivaldate": voyage.arrivaldate,
+                    "captain": voyage.captain,
+                    "copilot": voyage.copilot,
+                    "flight_service_manager": voyage.flight_service_manager,
+                    "flight_attendant": voyage.flight_attendant,
+                }
+            )
