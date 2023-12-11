eeb522a67836797c1848bed0c3801675d0f89464
Marvin <marvin120203@gmail.com>
2023-12-11 14:05:58 +0000

    update employee

--- src/UI/employees.py
+++ src/UI/employees.py
@@ -49,8 +49,7 @@ class Employees:
                 self.register_new_employee()
 
             elif command == "3" or command == "3.":
-                
-                pass  # eftir að implementa
+                self.update_employee()
             
             elif command == "4" or command == "4.":
                 pass  # eftir að implementa
@@ -392,6 +391,108 @@ class Employees:
                 ssid, name, rank, address, phone_nr, home_phone_nr = [
                     all_flight_attendant_information[i]
                     for i in range(0, (len(all_flight_attendant_information)))
+                ]
+                self.logic_wrapper.register_flight_attendant(
+                    FlightAttendant(
+                        ssid, name, "Cabincrew", rank, address, phone_nr, home_phone_nr
+                    )
+                )
+            else:
+                pass  # ERROR :)
+
+        elif command == "b" or command == "b.":
+            pass
+
+        elif command == "q" or command == "q.":
+            pass
+
+        else:
+            print(UIConstants.INVALID_INPUT)  # MAKE ERROR MSG PLS
+
+    def update_employee(self):
+        """This function prints a new menu for the user, which allows
+        them to either create a new pilot or flight attendant.
+        This will generate a list of all the information that the
+        user wants to list for the new employee and sends it over
+        to the employeelogic.py file for verification.
+        """
+        print(UIConstants.HEADER.format(UIConstants.REGISTER_NEW_EMPLOYEE))
+        print(
+            UIConstants.TWO_MENU_OPTION.format(
+                UIConstants.UPDATE_PILOT,
+                UIConstants.UPDATE_FLIGHT_ATTENDANT,
+                UIConstants.BACK,
+                UIConstants.QUIT,
+            )
+        )
+
+        # print("Choose an employee to register")
+        # print("")
+        command = input("User input: ")
+        if command == "1" or command == "1.":
+            print(UIConstants.HEADER.format(UIConstants.UPDATE_PILOT))
+            print(UIConstants.EMPLOYEE_INFORMATION_MESSAGE)
+            pilot_info_print = UIConstants.UPDATE_EMPLOYEE_INFO.split(", ")
+
+            update_pilot_information = []
+            for i in pilot_info_print:
+                print(f"{i}", end=" ")
+                pilot_information = input()
+                update_pilot_information.append(pilot_information)
+                print(update_pilot_information)
+
+            if len(update_pilot_information) == 6:
+                ssid, name, rank, address, phone_nr, license = [
+                    update_pilot_information[i]
+                    for i in range(0, (len(update_pilot_information)))
+                ]
+                self.logic_wrapper.register_pilot(
+                    Pilot(ssid, name, "Pilot", rank, address, phone_nr, license)
+                )
+            elif len(update_pilot_information) == 7:
+                ssid, name, rank, address, phone_nr, home_phone_nr, license = [
+                    update_pilot_information[i]
+                    for i in range(0, (len(update_pilot_information)))
+                ]
+                self.logic_wrapper.update_pilot(
+                    Pilot(
+                        ssid,
+                        name,
+                        "Pilot",
+                        rank,
+                        address,
+                        phone_nr,
+                        home_phone_nr,
+                        license,
+                    )
+                )
+            else:
+                pass  # ERROR :)
+
+        elif command == "2" or command == "2.":
+            print(UIConstants.HEADER.format(UIConstants.UPDATE_FLIGHT_ATTENDANT))
+            print(UIConstants.EMPLOYEE_INFORMATION_MESSAGE)
+            flight_attendant_info_print = UIConstants.REGISTER_EMPLOYEE_INFO.split(", ")
+            flight_attendant_info_print = flight_attendant_info_print[0:-2]
+
+            update_flight_attendant_information = []
+            for i in flight_attendant_info_print[0 : len(flight_attendant_info_print)]:
+                print(f"{i}", end=" ")
+                update_flight_attendant_information = input()
+                update_flight_attendant_information.append(update_flight_attendant_information)
+
+            if len(update_flight_attendant_information) == 5:
+                ssid, name, rank, address, phone_nr = [
+                    update_flight_attendant_information[i]
+                    for i in range(0, (len(update_flight_attendant_information)))
+                ]
+                self.logic_wrapper.update_flight_attendant(
+                    FlightAttendant(ssid, name, "Cabincrew", rank, address, phone_nr)
+                )
+            elif len(update_flight_attendant_information) == 6:
+                ssid, name, rank, address, phone_nr, home_phone_nr = [
+                    update_flight_attendant_information[i]
+                    for i in range(0, (len(update_flight_attendant_information)))
                 ]
                 self.logic_wrapper.register_flight_attendant(
                     FlightAttendant(
