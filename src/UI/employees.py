04c7f27064d9bd8faafdf6484f904bec735f990d
Marvin <marvin120203@gmail.com>
2023-12-12 11:33:31 +0000

    Update employees.py

--- src/UI/employees.py
+++ src/UI/employees.py
@@ -424,44 +424,6 @@ class Employees:
             home_phone_nr = input("Enter the new home phone number (optional): ")
             license = input("Enter the new license: ")
 
-            EmployeeData.update_pilot(ssid, rank)
-        elif employee_type == "2":
-            # Update flight attendant
-            rank = input("Enter the new rank: ")
-            address = input("Enter the new address: ")
-            phone_nr = input("Enter the new phone number: ")
-            home_phone_nr = input("Enter the new home phone number (optional): ")
-
-            EmployeeData.update_flight_attendant(ssid, rank, address, phone_nr, home_phone_nr)
-        else:
-            print("Invalid employee type selection.")
-
-        print("Employee information updated successfully.")
-
-        
-
-
-
-
-
-
-
-
-
-
-'''
-    def update_employee(self):
-        employee_type = input("Select the employee type to update:\n1. Update pilot\n2. Update flight attendant\n")
-        ssid = input("Enter the SSID of the employee you want to update: ")
-
-        if employee_type == "1":
-            # Update pilot
-            rank = input("Enter the new rank: ")
-            address = input("Enter the new address: ")
-            phone_nr = input("Enter the new phone number: ")
-            home_phone_nr = input("Enter the new home phone number (optional): ")
-            license = input("Enter the new license: ")
-
             EmployeeData.update_pilot(ssid, rank, address, phone_nr, home_phone_nr, license)
         elif employee_type == "2":
             # Update flight attendant
@@ -476,11 +438,6 @@ class Employees:
 
         print("Employee information updated successfully.")
 
-'''
-
-
-
-
-
+        
 
 
