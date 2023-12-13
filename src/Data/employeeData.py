655c396978ce1e96a06dc3404f38f6c858fac7ab
Marvin <marvin120203@gmail.com>
2023-12-13 13:48:18 +0000

    nevermind, search by not workin does not work exactly as I planned

--- src/Data/employeeData.py
+++ src/Data/employeeData.py
@@ -189,25 +189,13 @@ class EmployeeData:
 
             return ret_list
     
-    def search_by_day(self, filter):
-        ret_list = []
-        with open("src/Files/shift_plan.csv", newline="", encoding="utf-8") as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                if (
-                    str(filter) in row["shift_start_date"]
-                ):  # can use these param to search
-                    ret_list.append(row)
-        # returns the list of employees that match the search
-        return ret_list
-    
     def search_by_not_day(self, filter):
-        ret_list = []
-        with open("src/Files/shift_plan.csv", newline="", encoding="utf-8") as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                if str(filter) != row["shift_start_date"]:
-                    ret_list.append(row)
-                elif str(filter) == row["shift_start_date"]:
-                    ret_list = [r for r in ret_list if r["name"] != row["name"]] #if the person is working on that day remove the name from list
-        return ret_list
+         ret_list = []
+         with open("src/Files/shift_plan.csv", newline="", encoding="utf-8") as csvfile:
+             reader = csv.DictReader(csvfile)
+             for row in reader:
+                 if str(filter) != row["shift_start_date"]:
+                     ret_list.append(row)
+                 elif str(filter) == row["shift_start_date"]:
+                     ret_list = [r for r in ret_list if r["name"] != row["name"]] #if the person is working on that day remove the name from list
+         return ret_list 
