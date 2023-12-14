f1b37ce8e9d89154dd4c56efe99a5ebb42d487c7
Viggó <viggoh21@ru.is>
2023-12-14 17:37:38 +0000

    Fixed verify insignia issue, deleted old airplanes.csv

--- src/Logic/Verifications/verifyairplane.py
+++ src/Logic/Verifications/verifyairplane.py
@@ -1,3 +1,5 @@
+from Model.AirplaneModel import Airplane
+
 class InvalidInsigniaError(Exception):
     pass
 class InsigniaFormatError(Exception):
@@ -5,13 +7,11 @@ class InsigniaFormatError(Exception):
 class InsigniaExistsError(Exception):
     pass
 
-
 class VerifyAirplane:
-    def __init__(self, airplane_info, data):
+    def __init__(self, airplane_info: Airplane, all_airplane_data: list[Airplane]):
 
-        self.data = data
+        self.all_airplane_data = all_airplane_data
         self.airplane_info = airplane_info
-
 
     def verify_airplane_helper(self, info_type: str):
         """ info_type must be a string with the values 'insignia', 'plane_type', 'supplier', 'seats'.
@@ -20,18 +20,17 @@ class VerifyAirplane:
 
         in_use_info = []
 
-        for i in range(len(self.data)):
-            temp = self.data[i]
+        for i in range(len(self.all_airplane_data)):
+            temp = self.all_airplane_data[i]
             in_use_info.append(getattr(temp, info_type))    # getattr kallar á method 'info_type' í classa temp
 
         return in_use_info
-
 
     def verify_insignia(self):
 
         insignia = self.airplane_info.insignia.split("-")
         
-        if self.airplane_info.insignia.split()[2] != "-":
+        if list(self.airplane_info.insignia)[2] != "-":
             raise InsigniaFormatError()
 
         elif len(insignia) != 2:
@@ -45,7 +44,6 @@ class VerifyAirplane:
 
         elif self.airplane_info.insignia in self.verify_airplane_helper("insignia"):
             raise InsigniaExistsError()
-
 
     def validate_plane(self):
         self.verify_insignia()
